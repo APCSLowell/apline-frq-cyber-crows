07547b9c14eb052131851a9ec085643d0720fe82
cybercrow <malcolmbarrykao@gmail.com>
2025-04-21 12:40:00 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,16 +1,18 @@
 public class APLine
 {
-private int A,B,C 
-public APLine(int a, int b, int c)
+private int A,B,C; 
+public APLine(int a, int b, int c){
   A=a; 
   B=b;
   C=c; 
+}
 public double getSlope() {
   return ((double) -A)/B; 
 }
-public boolean isonLine(int x, int y)
+public boolean isonLine(int x, int y){
   if((A*x)+(b*y)+C==0)
     return true;  
   else 
     return false; 
+}
 }
